07cfb26ddd234543a32f288a1e6bb0a090e00f0f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-10-08 23:56:25 +0700

    Create app configuration

---
+++ config/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('dotenv').config();
+
+const config = {
+    development: {
+        host: process.env.SERVER_HOST,
+        port: process.env.SERVER_PORT,
+        routes: {
+            cors: true
+        },
+        secretKey: process.env.JWT_SECRET_KEY,
+        algorithms: process.env.JWT_ALGORITHMS,
+        expiresIn: process.env.JWT_EXPIRE_TIME,
+        tokenType: process.env.JWT_TOKEN_TYPE
+    },
+    test: {
+        host: 'localhost',
+        port: 3001,
+        secretKey: 'foobar'
+    },
+    production: {
+        host: process.env.SERVER_HOST,
+        port: process.env.SERVER_PORT,
+        secretKey: process.env.JWT_SECRET_KEY,
+        algorithms: process.env.JWT_ALGORITHMS,
+        expiresIn: process.env.JWT_EXPIRE_TIME,
+        tokenType: process.env.JWT_TOKEN_TYPE
+    }
+};
+
+const env = process.env.NODE_ENV || 'development';
+module.exports = config[env];
