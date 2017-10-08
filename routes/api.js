aa922192d43799b7c7668809ff7b22e7157f4d2e
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-10-09 00:07:37 +0700

    Create api routes

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Controllers   = require('../controllers');
+const Validation    = require('../validation');
+const Pkg           = require(__dirname + '/package.json');
+const prefixUrl        = '/';
+
+module.exports = [{
+    method: 'GET',
+    path: prefixUrl,
+    config: {
+        auth: false
+    },
+    handler: (request, reply) => {
+
+        return reply({
+            name: Pkg.name,
+            version: Pkg.version
+        });
+
+    }
+}
+
+];
